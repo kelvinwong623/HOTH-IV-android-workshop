a6a732806dd664bb6618a1adf70fc9c152a06f15
Kelvin Wong <kelvinwong623@gmail.com>
2018-02-15 17:42:46 -0800

    completed basic UI and implemented basic functionality

--- app/src/main/java/leafstudios/tipcalculator/MainActivity.java
+++ app/src/main/java/leafstudios/tipcalculator/MainActivity.java
@@ -2,21 +2,51 @@ package leafstudios.tipcalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button calculateButton;
+    EditText totalCostEdit;
+    EditText percentEdit;
+    TextView tipView;
+    TextView newTotalCostView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        calculateButton = (Button) findViewById(R.id.calculateButton);
+        totalCostEdit = (EditText) findViewById(R.id.totalCostEdit);
+        percentEdit = (EditText) findViewById(R.id.percentEdit);
+        tipView = (TextView) findViewById(R.id.tipView);
+        newTotalCostView = (TextView) findViewById(R.id.newTotalCostView);
+
+        calculateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double total = Double.parseDouble(totalCostEdit.getText().toString());
+                double percent = Double.parseDouble(percentEdit.getText().toString()) / 100.0 ;
+                double tip = calculateTip(total, percent);
+                double newTotal = calculateNewTotal(total, tip);
+
+                tipView.setText("Tip: $" + Double.toString(tip));
+                newTotalCostView.setText("New Total: $" + Double.toString(newTotal));
+            }
+        });
     }
 
-    protected double getTip(double total, double percent)
+    protected double calculateTip(double total, double percent)
     {
         return total / percent;
     }
 
-    protected double getNewTotal(double total, double tip)
+    protected double calculateNewTotal(double total, double tip)
     {
         return total + tip;
     }
